936726e1edc256c7ae8e98a45a43481f7fd59112
PryhodaAndrii <andrii.pryhoda.mitpa.2023@lpnu.ua>
2024-05-14 21:17:47 +0000

    test commit

---
+++ Site-main/js/burger.js
@@ -0,0 +1,6 @@
+const toggle = () => {
+  document.getElementById("line1").classList.toggle("toggle_line1");
+  document.getElementById("line2").classList.toggle("toggle_line2");
+  document.getElementById("line3").classList.toggle("toggle_line3");
+  document.getElementById("burger_nav").classList.toggle("toggle_nav");
+};
